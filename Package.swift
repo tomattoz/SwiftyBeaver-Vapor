5592350153ca470088e2f444a419162c3e90edfe
tomattoz <tomattoz@users.noreply.github.com>
2020-04-15 17:38:02 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "SwiftyBeaverVapor", targets: ["SwiftyBeaverVapor"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.6.0")
     ],
     targets: [
